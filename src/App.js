1e97227dc9f67e9be0898db072634b5ce99d321c
Gary J <github@accu.ca>
2021-12-07 19:50:16 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -7,25 +7,6 @@ export default function App() {
   const [text, setText] = useState("");
 
   const addItem = function() {
-    // const newList = [...list];
-    // newList.push(text);
-    // setList(newList);
-
-    // Add Item to the end
-    setList([...list, text]);
-
-    // Add Item to the beginning
-    setList([text, ...list]);
-  };
-
-  // Not the best Delete function
-  const deleteItem = function() {
-    // const newList = [...list];
-    // newList.pop();
-    // setList(newList);
-
-    // Remove tjhe last item - same as a pop
-    setList([...list].slice(0, -1));
   };
 
   const textChanged = function(event) {
@@ -33,12 +14,11 @@ export default function App() {
   };
 
   const itemList = list.map((item, i) => {
-    return <li key={i} onClick={deleteItem} >{item}</li>;
+    return <li key={i}>{item}</li>;
   });
 
   return (
     <div className="App">
-
       <Header text="My ToDo List" />
       <input value={text} onChange={textChanged} />
       <br />
