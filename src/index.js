9cc9532aab825387d2383883a548faf788f5dfde
Gary J <github@accu.ca>
2021-12-07 21:07:52 -0800

    Update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import ReactDOM from 'react-dom';
-import App from "App";
+
+// Webpack won't be able to detect changes to App if in index.js
+// Move App to its own App.js file for real development
+function App() {
+
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// JQuery equivalant
+// const app = <div>Hello World</div>;
+// $("#root").append(app)
